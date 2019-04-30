e036a48b8376e8f2d01e6442aaba70e01451fb5d
offorma <offorma@gmail.com>
2019-04-30 09:27:28 +0100

    added the board class

---
+++ board.rb
@@ -0,0 +1,21 @@
+def class Board
+   # cordinate = { "1" => [0,0], "2" => [0,1], "3" =>[0,2], "4" => [1,0], "5" => [1,1], "6" =>[1,2],"7" => [2,0], "8" => [2,1], "9" =>[2,2] }
+   def add_piece()
+
+   end
+   def show_board()
+
+   end
+   def board_full()
+
+   end
+   def diagonal_win()
+
+    end
+    def horizontal_win()
+    
+    end
+    def veritcal_win()
+    
+    end
+end
