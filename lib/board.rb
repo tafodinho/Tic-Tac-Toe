3595df86b583a4e428aba012ce3bc4b40be22427
offorma <offorma@gmail.com>
2019-04-30 23:02:22 +0100

    started the show board method

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,16 @@
 def class Board
+    def initialize()
+        @board = Array.new(3){Array.new(3)}
+    end
    # cordinate = { "1" => [0,0], "2" => [0,1], "3" =>[0,2], "4" => [1,0], "5" => [1,1], "6" =>[1,2],"7" => [2,0], "8" => [2,1], "9" =>[2,2] }
-   def add_piece()
+   def add_piece(position, piece)
 
    end
    def show_board()
+        @board.each_with_index do |row, index_outer|
+            row.each_with_index do |value, index_inner|
+                if value
+
 
    end
    def board_full()
