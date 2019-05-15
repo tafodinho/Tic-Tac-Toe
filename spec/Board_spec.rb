e84c7f25dc5dca50835da58679fa69c1b6d0b3fa
offorma <offorma@gmail.com>
2019-05-15 23:33:03 +0100

    changed context statements to reflect test purpose

--- spec/Board_spec.rb
+++ spec/Board_spec.rb
@@ -6,11 +6,11 @@ describe Board do
    describe '#add_piece' do
 
         let(:board) {Board.new}
-        
-        it 'returns true' do
+
+        it 'returns true when piece is added succesfully' do
             expect(board.add_piece(1,'X')).to eq (true)
         end
-        it 'returns false' do
+        it 'returns false when piece fails to be added' do
             board.add_piece(1,'X')
             expect(board.add_piece(1,'X')).to eq (false)
         end
