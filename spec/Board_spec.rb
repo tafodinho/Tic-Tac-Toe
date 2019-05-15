cb6efea1d1edbd5713025406c9d3da5828bfe3d0
offorma <offorma@gmail.com>
2019-05-15 23:30:17 +0100

    used let to make new board object

--- spec/Board_spec.rb
+++ spec/Board_spec.rb
@@ -4,14 +4,15 @@ require "./lib/Player"
 require"./lib/Board"
 describe Board do 
    describe '#add_piece' do
-      it 'returns true' do
-         board =  Board.new
-         expect(board.add_piece(1,'X')).to eq (true)
-      end
-      it 'returns false' do
-        board2 =  Board.new
-        board2.add_piece(1,'X')
-        expect(board2.add_piece(1,'X')).to eq (false)
-      end
+
+        let(:board) {Board.new}
+        
+        it 'returns true' do
+            expect(board.add_piece(1,'X')).to eq (true)
+        end
+        it 'returns false' do
+            board.add_piece(1,'X')
+            expect(board.add_piece(1,'X')).to eq (false)
+        end
    end
 end
