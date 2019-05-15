10f54a11f0427c2de42a58237ebc875ba735f270
offorma <offorma@gmail.com>
2019-05-15 23:16:46 +0100

    used let to make objects

--- spec/Game_spec.rb
+++ spec/Game_spec.rb
@@ -5,28 +5,28 @@ require"./lib/Board"
 
 describe Game do 
    describe '#winner?' do
-      it 'returns false' do
-         board =  Board.new
-         player_1 = Player.new("Player 1", "X", board)
-         player_2 = Player.new("Player 2", "O", board)
-         game = Game.new(board, player_1, player_2)
+
+      let(:board) { Board.new}
+      let(:player_1) {Player.new("Player 1", "X", board)}
+      let(:player_2) {Player.new("Player 2", "O", board)}
+      let(:game) {Game.new(board, player_1, player_2)}
+      
+      it 'returns false when there is no winner' do
          expect(game.is_game_over?).to eq (false)
       end
-      it 'returns true' do
-         board =  Board.new
-         player_3 = Player.new("Player 1", "X", board)
-         player_4 = Player.new("Player 2", "O", board)
-         player_3.moves = [0,1,2]
-         game = Game.new(board, player_3, player_4)
+      it 'returns true when there is a winner' do
+          player_1.moves = [0,1,2]
          expect(game.is_game_over?).to eq (true)
       end
    end
    describe '#play' do
-      it 'returns false' do
-         board =  Board.new
-         player_1 = Player.new("Player 1", "X", board)
-         player_2 = Player.new("Player 2", "O", board)
-         game = Game.new(board, player_1, player_2)
+
+      let(:board) { Board.new}
+      let(:player_1) {Player.new("Player 1", "X", board)}
+      let(:player_2) {Player.new("Player 2", "O", board)}
+      let(:game) {Game.new(board, player_1, player_2)}
+
+      it 'returns nil when execution is complete' do
          expect(game.play).to eq (nil)
       end
    end
