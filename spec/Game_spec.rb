58897a0ddcc596b1acbadb63fd43f9e7ffe21a9a
offorma <offorma@gmail.com>
2019-05-17 10:18:46 +0100

    refactored game test

--- spec/Game_spec.rb
+++ spec/Game_spec.rb
@@ -4,13 +4,13 @@ require "./lib/Player"
 require"./lib/Board"
 
 describe Game do 
-   describe '#winner?' do
 
-      let(:board) { Board.new}
-      let(:player_1) {Player.new("Player 1", "X", board)}
-      let(:player_2) {Player.new("Player 2", "O", board)}
-      let(:game) {Game.new(board, player_1, player_2)}
-      
+   let(:board) { Board.new}
+   let(:player_1) {Player.new("Player 1", "X", board)}
+   let(:player_2) {Player.new("Player 2", "O", board)}
+   let(:game) {Game.new(board, player_1, player_2)}
+
+   describe '#winner?' do 
       it 'returns false when there is no winner' do
          expect(game.is_game_over?).to eq (false)
       end
@@ -20,12 +20,6 @@ describe Game do
       end
    end
    describe '#play' do
-
-      let(:board) { Board.new}
-      let(:player_1) {Player.new("Player 1", "X", board)}
-      let(:player_2) {Player.new("Player 2", "O", board)}
-      let(:game) {Game.new(board, player_1, player_2)}
-
       it 'returns nil when execution is complete' do
          expect(game.play).to eq (nil)
       end
