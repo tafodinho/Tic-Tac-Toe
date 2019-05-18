e6a89142b632cb88b5cc134d309ef7f9a64578e0
offorma <offorma@gmail.com>
2019-05-18 16:16:11 +0100

    wrote test for winner? method

--- spec/Game_spec.rb
+++ spec/Game_spec.rb
@@ -10,7 +10,7 @@ describe Game do
    let(:player_2) {Player.new("Player 2", "O", board)}
    let(:game) {Game.new(board, player_1, player_2)}
 
-   describe '#winner?' do 
+   describe '#is_game_over?' do 
       it 'returns false when there is no winner' do
          expect(game.is_game_over?).to eq (false)
       end
@@ -21,7 +21,20 @@ describe Game do
    end
    describe '#play' do
       it 'returns nil when execution is complete' do
+         player_1.moves = [0,1,2]
          expect(game.play).to eq (nil)
+      end
+   end
+   describe '#Winner?' do
+      it 'returns true when there is a winning combination' do
+         player_1.moves = [0,1,2,4,5]
+         expect(game.winner?).to eq (true)
+      end
+   end
+   describe '#Winner?' do
+      it 'returns false when there is no winning combination' do
+         player_1.moves = [0,6,8,5]
+         expect(game.winner?).to eq (false)
       end
    end
 end
